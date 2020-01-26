83071c86db7c7d507944074446ae34a9cb022b06
denisegiubilei <giubileidenise@gmail.com>
2020-01-26 13:51:10 -0300

    adding fallback img

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import fallbackImg from '../../assets/fallback.jpg'
 
 const Question = ({ title, img }) => (
   <div className="img-container">
-    <img alt={title} src={img} />
+    <img 
+      alt={title} 
+      src={img} 
+      onError={e => { 
+          e.target.onerror = null
+          e.target.src = fallbackImg
+        }
+      }
+    /> 
     <p className="question">{title}</p>
   </div>
 )
